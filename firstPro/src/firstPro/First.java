2df4a69236f8cdb428c886c275724b8210c53738
rajeshkumar266 <myemail@example.com>
2024-10-13 20:58:13 +0530

    getting user inputs

---
+++ firstPro/src/firstPro/First.java
@@ -0,0 +1,12 @@
+package firstPro;
+
+public class First {
+	public static void main(String args[]) {
+		
+		int price=800;
+		System.out.println("hello guys");
+		
+		System.out.println(800);
+	}
+
+}
